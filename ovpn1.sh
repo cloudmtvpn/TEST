b4bc9a671ae06ac11acd378986ac048944a3e478
yarzardhiyit <34724260+yarzardhiyit@users.noreply.github.com>
2017-12-21 15:56:10 +0700

    Update ovpn1.sh

--- ovpn1.sh
+++ ovpn1.sh
@@ -30,14 +30,14 @@ vps="aneka";
 #if [[ $vps = "zvur" ]]; then
 	#source="http://"
 #else
-	source="https://raw.githubusercontent.com/syahz86/y"
+	source="https://raw.githubusercontent.com/yarzardhiyit/d8-x64"
 #fi
 
 # go to root
 cd
 
 # check registered ip
-wget -q -O IP $source/debian7/IP.txt
+wget -q -O IP $source/master/IP.txt
 if ! grep -w -q $MYIP IP; then
 	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
 	if [[ $vps = "zvur" ]]; then
@@ -54,14 +54,14 @@ apt-get -y --purge remove openvpn*;
 # install openvpn
 #myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar $source/debian7/openvpn.tar
+wget -O /etc/openvpn/openvpn.tar $source/master/openvpn.tar
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf $source/debian7/tcp56.conf
+wget -O /etc/openvpn/1194.conf $source/master/tcp56.conf
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf $source/debian7/iptables.conf
+wget -O /etc/iptables.conf $source/master/iptables.conf
 sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
 
 
@@ -72,7 +72,7 @@ service openvpn restart
 
 # configure openvpn client config
 cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn $source/debian7/tcp56.ovpn
+wget -O /etc/openvpn/client.ovpn $source/master/tcp56.ovpn
 
 cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
 sed -i 's/ipserver/$MYIP/g' /home/vps/public_html/client.ovpn
